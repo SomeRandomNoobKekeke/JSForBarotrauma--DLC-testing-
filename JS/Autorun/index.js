71b8606f6bd85fd950ca7b1adeaf3c41a310808c
SomeRandomNoobKekeke <SkritniyChel34617@proton.me>
2026-01-31 04:41:43 +0300

    runWith

---
+++ JS/Autorun/index.js
@@ -0,0 +1,7 @@
+JS.Global["runWith"] = function (expression, context) {
+  let keys = Object.keys(context);
+  let values = Object.values(context);
+  const func = new Function(...keys, expression);
+  return func(...values);
+}
+
